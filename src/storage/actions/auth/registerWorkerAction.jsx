bd312bf07e0e507cdf45a1601a4bb1fedfc4ccf1
basriibrahim1 <basxravier@gmail.com>
2023-03-25 17:33:19 +0700

    adding redux template

--- src/storage/actions/auth/registerWorkerAction.jsx
+++ src/storage/actions/auth/registerWorkerAction.jsx
@@ -3,15 +3,12 @@ import axios from "axios";
 export const RegisterWorkerAction = (data, navigate) => async (dispatch) => {
   try {
     dispatch({ type: "REGISTER_WORKER_PENDING" });
-    const result = await axios.post(
-      `${process.env.REACT_APP_BASE_URL}/auth/register-worker`,
-      data
-    );
+    const result = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/register-worker`, data);
     const worker = result.data.data;
     localStorage.setItem("name", worker.name);
     localStorage.setItem("email", worker.email);
     localStorage.setItem("phone", worker.phone);
-    localStorage.setItem("password", password.email);
+    localStorage.setItem("password", worker.email);
     dispatch({ type: "REGISTER_WORKER_SUCCESS", payload: worker });
     navigate("/add-profile-pekerja");
   } catch (err) {
